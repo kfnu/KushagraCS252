fe5ab71b80066539ff46b1e1d58b05b3fc11e61f
Kushagra <kushagra@Kushagras-MacBook-Pro.local>
2017-12-07 02:29:31 -0500

    Homepage UI

---
+++ src/headerguest.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kushagra
+ * Date: 12/7/17
+ * Time: 12:57 AM
+ */
+
+echo "<table width='100%' border='0' bgcolor='#ffffff'>";
+echo "<tr>";
+echo "<td width='100%' align='center'>";
+echo "<img src=img/Logomakr_0f71jB.png height='60' width='350' />";
+echo "</td>";
+/*echo "<td align='right'>";
+echo "<table width='100%' border='0'>";
+echo "<tr>";
+echo "<td><a href='createaccount.html'>Register</a></td>";
+echo "</tr>";
+echo "</table>";
+echo "</td>";*/
+echo "</tr>";
+echo "</table>";
+?>
--- src/index.php
+++ src/index.php
@@ -15,13 +15,16 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="css/guest.css">
     <script src="js/login.js"></script>
-    <title>Login</title>
+    <title>Homepage</title>
 </head>
 <body>
 <div class="header">
-
+    <?php include("headerguest.php");?>
 </div>
 <div class="login-page">
+    <p style="color:white;font-family: Roboto, sans-serif; font-size: 14px;">
+        Welcome to Calorie Counter...
+    </p>
     <div class="form">
         <!--<form class="register-form">
             <input type="text" placeholder="name"/>
@@ -31,10 +34,14 @@
             <p class="message">Already registered? <a href="#">Sign In</a></p>
         </form>-->
         <form class="login-form">
-            <input type="text" placeholder="username"/>
-            <input type="password" placeholder="password"/>
-            <button><a href="#">login</a></button>
-            <p class="message">Not registered? <a href="createaccount.html">Create an account</a></p>
+     <!--       <input type="text" placeholder="username"/>
+            <input type="password" placeholder="password"/>-->
+            <button><a href="login.html"><span  style="font-family: 'American Typewriter'" size="9px">Login</span></a></button>
+            <br/><br/>
+            <button><a href="createaccount.html"><span style="font-family: 'American Typewriter'" size="9px">Create Account</span></a></button>
+            <br/><br/>
+            <button><a href="contact.php"><span style="font-family: 'American Typewriter'" size="9px">Contact</span></a></button>
+            <!--<p class="message">Not registered? <a href="createaccount.html">Create an account</a></p>-->
         </form>
     </div>
 </div>
