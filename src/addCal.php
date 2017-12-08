9e98eb7875971b21bcd928183e6e7fc32c15869a
Kushagra <kushagra@pal-nat186-134-6.itap.purdue.edu>
2017-12-08 02:05:39 -0500

    Food Items and Add Calories UI

--- src/addCal.php
+++ src/addCal.php
@@ -10,6 +10,10 @@ $error=''; // Variable To Store Error Message
 //echo "username=".$_POST['username'];
 if (isset($_POST['submit'])) {
     //echo "username=".$_POST['username'];
+    echo "date = ".$_POST['caldate'];
+    echo "meal = ".$_POST['meal']."<br/>";
+    echo "item id = ".$_POST['item']."<br/>";
+    echo "quantity = ".$_POST['qua']."<br/>";
     if (empty($_POST['username']) || empty($_POST['password']) || empty($_POST['email'])) {
         $error = "Username or Password or Email is invalid";
     }
@@ -70,27 +74,81 @@ if (isset($_POST['submit'])) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="http://code.jquery.com/ui/1.11.0/themes/smoothness/jquery-ui.css">
+    <link href = "https://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css"
+          rel = "stylesheet">
+    <link rel="stylesheet" href="http://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <script src="http://code.jquery.com/jquery-3.2.1.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/calorie.css">
     <title>Create Account</title>
+    <script>
+        $(function() {
+            $("#caldate").datepicker({changeMonth: true, changeYear: true, defaultDate: '1/1/2018', dateFormat: 'mm/dd/yy' });
+        });
+    </script>
 </head>
-<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
+
 <body>
-<div ng-app="">
+<!--<div ng-app="">
     <p>Name : <input type="text" ng-model="name"></p>
     <h1>Hello {{name}}</h1>
-</div>
+</div>-->
 
 <div class="header">
-    <?php include("headerguest.php");?>
+    <?php include("headeruser.php");?>
 </div>
+
 <div class="login-page">
     <p style="color:white;font-family: Roboto, sans-serif; font-size: 14px;height:20px;">
         &nbsp;
     </p>
+
+
     <div class="form">
-        <form class="login-page" method="post">
-            <input type="text" name="username" placeholder="name"/>
+
+
+        <form class="login-page" method="post"> <!--<div class = "datepickerdemo" ng-controller = "dateController as ctrl"
+                 layout = "column" ng-cloak>-->
+            <input type="text" style='width:108px;background-color: #fffbcc;' name="caldate" value="<?=$_POST['caldate']?>" id="caldate">&nbsp;(mm/dd/yyyy)
+            </br></br>
+            <select name="meal" value="<?php echo $_POST["meal"]; ?>">
+                <option value="">Select...</option>
+                <option value="B">Breakfast</option>
+                <option value="L">Lunch</option>
+                <option value="D">Dinner</option>
+                <option value="O">Other</option>
+            </select>
+            <?php
+            $db_host = "us-cdbr-iron-east-05.cleardb.net";
+            $db_user = "bf980adbdc50be";
+            $db_pass = "867b73f4";
+            $db_name = "heroku_5945d53bb9e3d51";
+            $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+            $query = "SELECT id, CONCAT(item, ' (',unit,')') AS itemMod FROM fooditem ORDER By item ASC;";
+            //$result = conn($query);
+            //if (($result)||(mysql_errno == 0))
+            $result = mysqli_query($conn,$query);
+            //$result = $mysqli->query($query);
+            if ($result) {
+                echo "<select name='item' id='item' value=".$_POST["item"].">";
+                echo "<option value='" . "0" . "' selected=\"selected\"'>" . "Select" . "</option>";
+                while ($row = mysqli_fetch_array($result)) {
+                    echo "<option value='" . $row['id'] . "'>" . $row['itemMod'] . "</option>";
+                }
+                echo "</select>";
+            }
+            mysqli_close($conn);
+            ?>
+
+            <input style="border-color: #3B0B17; border-width: thick;" type="text" name="qua" value="<?php echo $_POST["qua"]; ?>" placeholder="quantity"/>
+            <input name="submit" type="submit" value=" Add ">
+        </form>
+            <!--<input type="text" name="username" placeholder="name"/>
             <input type="password" name="password" placeholder="password"/>
             <input type="text" name="email" placeholder="email address"/>
             <input name="submit" type="submit" value=" Create ">
