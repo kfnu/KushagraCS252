cd1ac6235cd1c23a60f23266b4b937e106492fe6
Kushagra <kushagra@Kushagras-MacBook-Pro.local>
2017-12-08 11:52:47 -0500

    Some adjustments

--- src/foodlist.php
+++ src/foodlist.php
@@ -27,7 +27,7 @@ if (!$query) {
 ?>
 <html>
 <head>
-    <link rel="stylesheet" href="css/headeruser.css">
+    <link rel="stylesheet" href="css/calorie.css">
     <title>Add Up Your Calories</title>
     <style type="text/css">
         body {
